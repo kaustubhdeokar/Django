febedc16fc661d9f0051b602652a0d3f873244b0
kaustubhdeokar <kaustubhdcpo@gmail.com>
2019-03-17 18:32:48 +0530

    add_users

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -9,3 +10,8 @@ class UserProfile(models.Model):
     website = models.URLField(default='')
     phone = models.IntegerField(default=0)
 
+def create_profile(sender,**kwargs):
+    if kwargs['created']:
+        user_profile=UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
